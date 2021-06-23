e9c54b4475e8795b3fed956d31c928359be22909
themoon <leeyejik99@gmail.com>
2021-06-23 23:57:35 +0900

    login input Add, bottom Text Add

--- ContentView.swift
+++ ContentView.swift
@@ -13,12 +13,12 @@ struct ContentView: View {
             Text("Meal")
                 //.fontWeight(.bold) // bold 표시
                 .foregroundColor(.black) // font Color Black 지정
-                .font(.system(size:50)) // font size change
+                .font(.system(size:55)) // font size change
                 .kerning(3) // 문자간 간격 설정
-                .padding(.top, 40) // 임시
                 //.font(.title)
             NavigationView{
-                VStack{/*
+                VStack{
+                    /*
                     NavigationLink(destination: ContentView()){
                         Text("네이버로 로그인")
                             .font(.system(size:20))
@@ -51,46 +51,65 @@ struct ContentView: View {
                             .background(Color.gray)
                             .foregroundColor(Color.white)
                     }*/
-                    Button(action: {}) {
-                      Text("네이버로 로그인")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(width: 300, height: 50)
-                        .background(Color.green)
-                        .cornerRadius(15.0)
+                    
+                    NavigationLink(destination: ContentView()){
+                        Text("시작하기")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            //.frame(width: 200, height: 50)
+                            .background(Color.gray)
+                            .cornerRadius(30.0)
                     }
-                    Button(action: {}) {
-                      Text("페이스북으로 로그인")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(width: 300, height: 50)
-                        .background(Color.blue)
-                        .cornerRadius(15.0)
+                    
+                    Spacer()
+                    
+                    VStack(){
+                        Button(action: {}) {
+                          Text("네이버 계정으로 시작하기")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width: 300, height: 50)
+                            .background(Color.green)
+                            .cornerRadius(4.0)
+                        }
+                        Button(action: {}) {
+                          Text("페이스북으로 시작하기")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width: 300, height: 50)
+                            .background(Color.blue)
+                            .cornerRadius(4.0)
+                        }
+                        Button(action: {}) {
+                          Text("카카오톡으로 시작하기")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width: 300, height: 50)
+                            .background(Color.yellow)
+                            .cornerRadius(4.0)
+                        }
+                        Button(action: {}) {
+                          Text("구글로 시작하기")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width: 300, height: 50)
+                            .background(Color.gray)
+                            .cornerRadius(4.0)
+                        }
                     }
-                    Button(action: {}) {
-                      Text("카카오톡으로 로그인")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(width: 300, height: 50)
-                        .background(Color.yellow)
-                        .cornerRadius(15.0)
-                    }
-                    Button(action: {}) {
-                      Text("구글로 로그인")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(width: 300, height: 50)
-                        .background(Color.gray)
-                        .cornerRadius(15.0)
+                    Spacer()
+                    Text("계정이 없으신가요?")
+                    Button("가입하기"){
+                        print("가입하기")
                     }
                 }
             }
         }
-        
     }
 }
 
